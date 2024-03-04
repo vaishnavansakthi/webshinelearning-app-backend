63cca72c87aa8c935a108f1422641884c6579379
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-04 10:50:15 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,6 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
     ],
   });
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
