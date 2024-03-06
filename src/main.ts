698abde85d79c4f47830d2b5cd0b3f60642b4380
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-06 20:56:42 +0530

    update the issue

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({ credentials: true });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Webshinelearning portal API')
