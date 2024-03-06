8e18249c7f9fe2f050a6ee3a240017c9dc7e34b8
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-06 22:00:18 +0530

    test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { AuthController } from "./auth.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthEntity } from "./model/auth.entity";
 import { JwtModule, JwtService } from "@nestjs/jwt";
+import { CorsModule } from "@nestjs/platform-express"
 
 @Module({
-    imports: [TypeOrmModule.forFeature([AuthEntity]), JwtModule.register({
+    imports: [CorsModule.forRoot({
+        origin: 'http://localhost:5173',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+      }),TypeOrmModule.forFeature([AuthEntity]), JwtModule.register({
         global: true,
         secret: process.env.JWT_SECRET,
         signOptions: { expiresIn: '1d' }
