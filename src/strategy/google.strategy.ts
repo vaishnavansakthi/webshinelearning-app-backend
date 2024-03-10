8ac14427acf4be9075f6e62b31bf24cec8dc23e3
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-10 18:06:00 +0530

    adding env

--- src/strategy/google.strategy.ts
+++ src/strategy/google.strategy.ts
@@ -6,9 +6,9 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: '351460226914-1qesepveqt5qpu24ka9cba47hhr0ljhq.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: 'http://localhost:3333/auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       passReqToCallback: true,
       scope: ['profile', 'email'],
     }); 
