1c0356a4f5b5791b41d4e780e79ba11963ef648a
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-03-20 01:27:59 +0530

    removed port

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,6 @@ import { TaskModule } from './task/task.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DATABASE'),
